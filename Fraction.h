a8d5b40cbca21c2059f927719cec9ecc1f684538
Beqa149 <baramiabeqa1@gmail.com>
2017-09-28 21:03:09 +0400

    Update Fraction.h

--- Fraction.h
+++ Fraction.h
@@ -5,15 +5,20 @@ class Fraction
 private:
 	int Denominator;
 	int Numerator;
+
+	int GCD(int, int);
+
 public:
 	Fraction();
 	Fraction(int Denominator, int Numerator);
 	Fraction(const Fraction &);
-	int GCD(int, int);
 	void Normalize();
 	void Show();
 	Fraction Add(Fraction _fraction);
 	Fraction Subtract(Fraction _fraction);
 	Fraction Divide(Fraction _fraction);
 	Fraction Multiply(Fraction _fraction);
+	~Fraction();
+	//Overloaded Class Operator
+	Fraction operator+(const Fraction &);
 };
