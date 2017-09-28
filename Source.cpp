cab2a65558c31fcb5933e3077b88639e9ff12ce5
Beqa149 <baramiabeqa1@gmail.com>
2017-09-28 21:03:42 +0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -7,20 +7,12 @@ using namespace std;
 int main()
 {
 
-	Fraction f2(3, 5);
-	Fraction f3(7, 2);
+	Fraction f2(8,16);
+	Fraction f3(7,21);
 	f2.Show();
 	f3.Show();
-
-	Fraction result = f2.Add(f3);
-	result.Show();
-	result = f2.Subtract(f3);
-	result.Show();
-	result = f2.Divide(f3);
-	result.Show();
-	result = f2.Multiply(f3);
-	result.Show();
-
+	Fraction c = f2 + f3;
+	c.Show();
 	cin.get();
 	return 0;
 
